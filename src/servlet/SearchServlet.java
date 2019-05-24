357eae8eec00ad3c539ce85c15ab7dae1e7022c0
sajikdan <49645011+sajikdan@users.noreply.github.com>
2019-05-24 11:57:11 +0200

    Update SearchServlet.java

--- src/servlet/SearchServlet.java
+++ src/servlet/SearchServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class NewServlet
+ * Servlet implementation class SearchServlet
  */
 @WebServlet("/SearchServlet")
-public class NewServlet extends HttpServlet {
+public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NewServlet() {
+    public SearchServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
