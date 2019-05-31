531ee4d66d0d9bc6c42dbe6dcaee657794dbfdd4
PanYan1998 <48472734+PanYan1998@users.noreply.github.com>
2019-05-31 10:04:58 +0200

    Create jump.js

---
+++ WebContent/js/jump.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+	$( '#loginWindow' ).animate({ 'width': '100%' }, 500)
+    .delay(30)
+    .animate({ 'height': '500px' }, 500);
+  $( '.page-header, .input-group, .btn' )
+    .delay(850)
+    .animate({ 'opacity': '100' }, 7000);
+});
