62fcc83e7fa9c7ad9741ac2ef336778803f4437a
sajikdan <49645011+sajikdan@users.noreply.github.com>
2019-06-01 15:17:34 +0200

    Servlets

--- HomeX/src/servlet/DeleteHouseServlet.java
+++ HomeX/src/servlet/DeleteHouseServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.User;
+import control.Administrator;
 import control.Authentication;
 
 /**
@@ -52,11 +56,44 @@ public class DeleteHouseServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
-		
+        HttpSession session = request.getSession();
+        String errorMessage;
+        boolean hasError = true;
+        
+        Authentication authentication = (Authentication)session.getAttribute("authentication");
+        Administrator administrator = (Administrator)session.getAttribute("administrator");
+   
+        User user = authentication.getUser();
+        
+        Set<Integer> house = user.getHouse();
+        Iterator<Integer> itr = house.iterator();
+        
 		String houseId = (String)request.getParameter("houseId");
         int id = Integer.parseInt(houseId);
 
-        String available = (String)request.getParameter("available");
+        while(itr.hasNext()) {
+        	if(itr.next() == id) {
+        		itr.remove();
+        		hasError = false;
+        		break;
+        	}
+        }
+
+        if(!administrator.deleteHouse(id)) { //if deletion unsuccessful
+        	hasError = true;
+        }
+        
+        if(hasError) { //if there's an error
+        	errorMessage = "Error occured while deleting house!\n";
+        	request.setAttribute("errorMessage", errorMessage);
+        	RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("profile.jsp");
+        	dispatcher.forward(request, response);
+        }
+        else {
+        	session.setAttribute("administrator", administrator);
+        	RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("profile.jsp");
+        	dispatcher.forward(request, response);
+        }
 	}
 
 }
